0e955feda47f9f937ce0e541b0298cab62428d32
shasidu <shasidumalshan9579@gmail.com>
2024-11-20 00:05:43 +0530

    fiveth commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.assignment.vehiclelicensepointsys.service.vehiclelicensepointsystem {
     requires javafx.fxml;
     requires java.sql;
     requires static lombok;
+    requires com.google.protobuf;
+    requires mysql.connector.j;
 
     opens com.assignment.service.Controller to javafx.fxml;
     opens com.assignment.service.Dto to javafx.base;
